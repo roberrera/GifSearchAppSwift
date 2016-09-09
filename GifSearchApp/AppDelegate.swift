e65c05d2368234ab7c04369b5fe13fdd598dd988
roberrera <roberterrera@gmail.com>
2016-09-09 19:38:11 -0400

    Add a UICollectionViewController

--- GifSearchApp/AppDelegate.swift
+++ GifSearchApp/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window?.tintColor = themeColor
         return true
     }
 
